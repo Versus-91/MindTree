0912990db6d704d53a8386d5a39a0ddf0f75e8a2
versus-91 <gharibyan.ghader@gmail.com>
2026-05-29 12:20:00 +0200

    Adding Cart use case

---
+++ src/Application/Interfaces/Data/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Data
+{
+    public interface ICartRepository
+    {
+        Task<Cart?> GetByCustomerIdAsync(Guid customerId);
+        Task SaveAsync(Cart shoppingCart);
+    }
+}
---
+++ src/Domain/Entities/Cart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Cart(Guid customerId)
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Guid CustomerId { get; } = customerId;
+
+        public IReadOnlyCollection<CartItem> Items =>
+             _items.AsReadOnly();
+
+        private readonly List<CartItem> _items = new();
+
+        public void AddItem(Guid productId, string productName, decimal
+                            productPrice, int quantity)
+        {
+            var existingItem = _items.SingleOrDefault(i => i.ProductId ==
+                                                      productId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                _items.Add(new CartItem(productId, productName,
+                           productPrice, quantity));
+            }
+        }
+    }
+}
