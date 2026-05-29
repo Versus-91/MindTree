2ec0eff112b72b0b5410d3ee7235df11dd9a7c3a
versus-91 <gharibyan.ghader@gmail.com>
2026-05-29 10:20:00 +0200

    Product use case

---
+++ src/Application/Interfaces/Data/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces.Data
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetAsync(Guid id);
+        Task UpdateAsync(Product product);
+    }
+}
---
+++ src/Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Domain.Entities;
+public class Product(string name, decimal price, int stockLevel, string
+                        imageUrl)
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string Name { get; } = name;
+    public decimal Price { get; } = price;
+    public int StockLevel { get; private set; } = stockLevel;
+    public string ImageUrl { get; } = imageUrl;
+
+    public void UpdateStockLevel(int stockLevel)
+    {
+        if (stockLevel < 0)
+        {
+            throw new ArgumentException("Invalid Stock Level.",
+                                        nameof(stockLevel));
+        }
+
+        StockLevel = stockLevel;
+    }
+}
+
