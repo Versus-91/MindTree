c40ba4fbc3cd473a8ab163c9b0eb06d98380f2e0
versus-91 <gharibyan.ghader@gmail.com>
2026-05-30 16:20:00 +0200

    Adding table component

---
+++ CleanCart/app/components/TableComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+            <tr>
+                <th v-for="header in headers">{{ header }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="row in rows">
+                <td v-for="header in headers" @click="notifySelect(row[header])">{{ row[header] }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{ rows: Record<string, unknown>[], headers: string[] }>(), {
+
+    rows: () => [{ 'one': 1 }, { 'two': 2 }]
+});
+const emit = defineEmits(['selected'])
+function notifySelect(value: unknown): void {
+    emit('selected', value)
+}
+</script>
+
+<style scoped></style>
--- CleanCart/app/pages/index.vue
+++ CleanCart/app/pages/index.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        {{ flag }}
+        <LazyTableComponent @selected="show" :rows="[{ 'sd': 2 }]" :headers="['sd']"></LazyTableComponent>
+        Switch: {{ flag }}
     </div>
 </template>
 
 <script setup lang="ts">
-const flag = false;
+const flag = ref(false);
+
+function show(e: unknown) {
+    alert(e);
+    flag.value = !flag.value;
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
